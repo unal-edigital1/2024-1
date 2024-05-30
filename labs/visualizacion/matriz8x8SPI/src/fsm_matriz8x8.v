1bcd48c23a59211864ec1a17fd69cce029adf81a
Fabeltranm <Ferney.Beltran@gmail.com>
2024-05-29 22:56:22 -0500

    add sp

--- labs/visualizacion/matriz8x8SPI/src/fsm_matriz8x8.v
+++ labs/visualizacion/matriz8x8SPI/src/fsm_matriz8x8.v
@@ -8,7 +8,7 @@ module fsm_matriz8x8 (
 
   
 );
-
+/*TODO: VALIDAR  QUE EL CS SE ACIVE PARA LOS 16 BITS  Y NO SOO OCHO */
 
    reg [7:0] data_in;
    wire [7:0] data_out;
@@ -17,7 +17,7 @@ module fsm_matriz8x8 (
    wire busy;
    wire avail;
   
-   wire [15:0] div_factor = 200000;  // Factor de división fijo
+   wire [25:0] div_factor = 25000000;  // Factor de división fijo
 
 	spi_master spi (
        .clk(clk),
@@ -34,23 +34,47 @@ module fsm_matriz8x8 (
    );
   
    
-	reg [7:0] memory4send [0:9];
+	reg [7:0] memory4CommandSend [0:30];
 
-	reg [3:0] mem_index = 0;
+	reg [5:0] mem_index = 0;
 	 
 	reg sendByte;
 	reg [10:0]state_send=0;
 
 	
 	initial begin
-        memory4send[0] = 8'h0C;
-        memory4send[1] = 8'h01;
-        memory4send[2] = 8'h09;
-        memory4send[3] = 8'h00;
-        memory4send[4] = 8'h00;
+        memory4CommandSend[0] = 8'h0C;  // Shutdown command
+        memory4CommandSend[1] = 8'h01;  // Shutdown Normal Operation
+        memory4CommandSend[2] = 8'h09;  // Decode-Mode
+        memory4CommandSend[3] = 8'h00;  // Decode-Mode No decode for digits 7–0
+		  memory4CommandSend[4] = 8'h0A;  // Intensity
+		  memory4CommandSend[5] = 8'h0A;  // Intensity value
+        memory4CommandSend[6] = 8'h0B;  // Scan Limit
+        memory4CommandSend[7] = 8'h07;  // Scan Limit Value
+		  memory4CommandSend[8] = 8'h0F;  // Display Test 
+        memory4CommandSend[9] = 8'h00;  // Display Test Value
 		  
+		  
+		  memory4CommandSend[10] = 8'h02;  // 
+        memory4CommandSend[11] = 8'hFF;  // 
+        memory4CommandSend[12] = 8'h03;  // 
+        memory4CommandSend[13] = 8'h7f;  // 
+        memory4CommandSend[14] = 8'h04;  // 
+        memory4CommandSend[15] = 8'h3c;  // 
+        memory4CommandSend[16] = 8'h05;  // 
+        memory4CommandSend[17] = 8'h55;  // 
+        memory4CommandSend[18] = 8'h06;  // 
+        memory4CommandSend[19] = 8'hAA;  // 
+        memory4CommandSend[20] = 8'h07;  // 
+        memory4CommandSend[21] = 8'h0f;  // 
+        memory4CommandSend[22] = 8'h08;  // 
+        memory4CommandSend[23] = 8'hf0;  //
+        memory4CommandSend[24] = 8'h01;  //  Digital 1
+        memory4CommandSend[25] = 8'hff;  // 
+	  
     end
 	
+	reg [1:0] c2=0;
    always @(posedge clk) begin
 		 if (~reset) begin
            state_send <= 0;
@@ -59,7 +83,7 @@ module fsm_matriz8x8 (
        end else begin
 		 case (state_send)
 			0:	begin
-				data_in <= memory4send[mem_index]; 
+				data_in <= memory4CommandSend[mem_index]; 
 				sendByte <=1; 
 				state_send <= 1;
 				end
@@ -71,11 +95,16 @@ module fsm_matriz8x8 (
 				if (avail) begin
 					state_send <= 0;	
    				mem_index <= mem_index+1;
-					if (mem_index > 3)
-						  mem_index <= 0;
-					
-				end			
+					if (mem_index > 24)begin
+						  mem_index <= 10;	
+						 state_send <= 3;	
+  
+					end
 				end
+				end
+			3: begin 
+				  mem_index <= 0;			
+			end 
 			endcase
 			end
 	end
